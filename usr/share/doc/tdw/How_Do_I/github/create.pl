ec4d91727b32f0ed93953baf5e39f2a2b2c39e0f
Troy Will <troydwill@gmail.com>
2010-06-26 18:59:01 -0700

    minor

---
+++ usr/share/doc/tdw/How_Do_I/github/create.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+my $API = `cat API`;
+chomp $API;
+
+print `curl -F 'login=troywill' -F "token=$API" http://github.com/api/v2/json/user/show/troywill`;
+
