5f8615d186fe742f813e70e426e472ad15a1573d
Troy Will <troydwill@gmail.com>
2010-07-10 18:14:16 -0700

    minor

--- usr/share/doc/tdw/How_Do_I/github/create.pl
+++ usr/share/doc/tdw/How_Do_I/github/create.pl
@@ -5,7 +5,7 @@ use strict;
 my $API = `cat API`;
 chomp $API;
 
-print `curl -F 'login=troywill' -F "token=$API" http://github.com/api/v2/yaml/repos/create/ -F 'name=getfit-perl' -F 'description='Asus Perl'`;
+print `curl -F 'login=troywill' -F "token=$API" http://github.com/api/v2/yaml/repos/create/ -F 'name=getfit-perl' -F 'description=Asus Perl'`;
 # description     => repo description
 # homepage    => homepage url
 # public      => 1 for public, 0 for private
