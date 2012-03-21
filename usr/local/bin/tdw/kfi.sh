db3ad5af368e5a99504b2c28dbf7ec32c88f9dbf
Troy Will <troydwill@gmail.com>
2012-03-21 08:13:20 -0700

    kfi.pl

--- usr/local/bin/tdw/kfi.sh
+++ usr/local/bin/tdw/kfi.sh
@@ -8,5 +8,4 @@ URL="http://p2.${STATION}.ccomrcdn.com/player/player_dispatcher.html?section=rad
 
 # mplayer -novideo 'rtmp://cp19771.live.edgefcs.net/live/Los_CA_KFI-AM_OR@s7652?auth=daEcYaldKa8bZc_bXatcddobca5dca8cXaT-bpxHD.-4q-LLYY6_1omFDov1LCqmzsyp&amp;aifp=1234&amp;CHANNELID=177&amp;CPROG=_&amp;MARKET=LOSANGELES-CA&amp;REQUESTOR=KFI-AM&amp;SERVER_NAME=p2.kfi-am.ccomrcdn.com&amp;SITE_ID=616&amp;STATION_ID=KFI-AM&amp;MNM=1&amp;TYPEOFPLAY=0" backup_location=""'
 
-mplayer -novideo 'rtmp://cp19771.live.edgefcs.net/live/Los_CA_KFI-AM_OR@s7652?auth=daEcYaldKa8bZc_bXatcddobca5dca8cXaT-bpxHD.-4q-LLYY6_1omFDov1LCqmzsyp&amp;aifp=1234&amp;CHANNELID=177&amp;CPROG=_&amp;MARKET=LOSANGELES-CA&amp;REQUESTOR=KFI-AM&amp;SERVER_NAME=p2.kfi-am.ccomrcdn.com&amp;SITE_ID=616&amp;STATION_ID=KFI-AM&amp;MNM=1&amp;TYPEOFPLAY=0" backup_location=""'
-
+mplayer -novideo 'rtmp://cp19771.live.edgefcs.net/live/Los_CA_KFI-AM_OR@s7652?auth=daEakcKd1bUaIavcOcTadbGbvayd9cscId7-bpAEpw-4q-LN3Y3_7npDBpm3HAqkxsxp&amp;aifp=1234&amp;CHANNELID=177&amp;CPROG=_&amp;MARKET=LOSANGELES-CA&amp;REQUESTOR=KFI-AM&amp;SERVER_NAME=p2.kfi-am.ccomrcdn.com&amp;SITE_ID=616&amp;STATION_ID=KFI-AM&amp;MNM=1&amp;TYPEOFPLAY=0" backup_location=""'
