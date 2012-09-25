7d7501cdd510d4944fccb2630f348df9b25dee34
Troy Will <troydwill@gmail.com>
2012-09-25 08:41:06 -0700

    new file:   org-find.pl

---
+++ usr/local/bin/tdw/org-find.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+my $search_term = $ARGV[0] or die;
+my $search_directory = "$ENV{HOME}/org";
+
+&search_filenames( $search_term );
+&grep_files( $search_term );
+
+sub search_filenames {
+  my $search_term = shift;
+  chdir( $search_directory );
+  print "=> $search_directory <=\n";
+  print "File names\n", "-" x 40, "\n";
+  print `find . | grep -i $search_term`;
+}
+
+sub grep_files {
+  my $search_term = shift;
+  chdir( $search_directory );
+  print "\nGrepping ...\n", "-" x 40, "\n";
+  print `grep -nir $search_term`;
+}
