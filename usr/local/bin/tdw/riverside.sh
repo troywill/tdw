4ee8d4fd5fe48a8a50a8dc24425c9083ff8be675
Troy Will <troydwill@gmail.com>
2012-09-25 10:02:10 -0700

    modified:   riverside.sh

--- usr/local/bin/tdw/riverside.sh
+++ usr/local/bin/tdw/riverside.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo -n "Riverside" && ./weather.pl "Riverside, California"  | grep fahr | grep -v dew | grep -v temper
-echo -n "San Gabriel" && ./weather.pl "San Gabriel, California"  | grep fahr | grep -v dew | grep -v temper
-echo -n "Northfield" && ./weather.pl "Northfield, Minnesota"  | grep fahr | grep -v dew | grep -v temper
+echo -n "Riverside" && weather.pl "Riverside, California"  | grep fahr | grep -v dew | grep -v temper
+echo -n "San Gabriel" && weather.pl "San Gabriel, California"  | grep fahr | grep -v dew | grep -v temper
+echo -n "Northfield" && weather.pl "Northfield, Minnesota"  | grep fahr | grep -v dew | grep -v temper
