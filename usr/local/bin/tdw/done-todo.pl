df16ab403eeccfbbccd3f2a4b70b4e9d6a3cc82c
Troy Will <troydwill@gmail.com>
2012-11-12 12:11:54 -0800

    ping-alive.pl A simple program to change DONEs to TODOs in Emacs org mode files

---
+++ usr/local/bin/tdw/done-todo.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+chomp(my $file = $ARGV[0]);
+my $backup = $file . 'bak';
+my $regexp = '\* DONE';
+my $replacement = '* TODO';
+open(my $oldorg, "<", $file)      # open for update
+    or die "Can't open $file for reading: $!";
+open(my $neworg, ">", $backup)
+    or die "Can't open $backup for writing: $!";
+while (<$oldorg>) {
+    s/$regexp/$replacement/;
+    print $neworg $_ or die "can't write $neworg: $!";
+}
+
+close($oldorg)            or die "can't close $oldorg: $!";
+close($neworg)            or die "can't close $neworg: $!";
+rename($backup, $file)   or die "can't rename $backup $file: $!";
+
